b7569a311f6e9543cbd62a045500921f394010c2
Thiti Dev <thiti.mwk.main@gmail.com>
2020-08-19 22:55:30 +0700

    finished function

---
+++ function.js
@@ -0,0 +1,30 @@
+
+//
+// ─── OLD SCHOOL ─────────────────────────────────────────────────────────────────
+//
+
+const name = "aaw0kenn"
+function sendWelcomeMsg(recieved_msg){
+    console.log("Welcome back "+recieved_msg)
+}
+sendWelcomeMsg(name)
+
+var sayGoodBye = function(msg){
+    console.log(msg)
+}
+sayGoodBye("Goodbye " + name)
+
+
+
+//
+// ─── ES6 ────────────────────────────────────────────────────────────────────────
+//
+const whatIsMySecretCode = () => {
+    console.log("X64")
+}
+whatIsMySecretCode()
+
+
+// Lol, why do I have to remind these things , they are already in my head
+// NVM , just wanna increase up my confidence
+// [Talking to myself again]
